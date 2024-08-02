52b2baf156dd1896821f83d47d63d1421f9fd6be
abdulrahmanabualmagd <abdulrahmanabualmagd@gmail.com>
2024-08-02 23:22:50 +0300

    Update

--- front/vite.config.js
+++ front/vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/login",
+    base: "/",
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src/"), // => import { index... } from '@/components';
